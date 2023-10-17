abcbc27afe53fd3dcb782da0c7a9550453c73ca1
vrao79 <vrao79@gatech.edu>
2023-10-17 14:52:22 -0400

    Spawning conatiners with python API. Issue #18

---
+++ code/docker/spawnContainer.py
@@ -0,0 +1,28 @@
+import docker
+
+# API Refrence: https://docker-py.readthedocs.io/en/stable/containers.html
+
+# Create a Docker client
+client = docker.from_env()
+
+
+def spawnContainer(image, container_name, command, environment=[], ports={}, volumes=[]):
+    # Define container options (optional)
+    container_options = {
+        "detach": True,    # Run the container in the background
+        "tty": True,       # Allocate a pseudo-TTY
+        "name": container_name,
+        "environment": environment,
+        "ports": ports,
+        "volumes": volumes,
+        "command": command
+    }
+
+    # Create and start the container
+    try:
+        container = client.containers.run(image, **container_options)
+        print(f"Container {container.name} is running.")
+    except docker.errors.ImageNotFound:
+        print(f"Image {image} not found. Make sure it exists.")
+    except docker.errors.APIError as e:
+        print(f"Error starting container: {e}")
