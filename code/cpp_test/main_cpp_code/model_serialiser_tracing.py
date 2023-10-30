3716e058e0b677c0619121f68f0063584d1729be
The-Death-Reaper <mayurpeshve2@gmail.com>
2023-10-30 19:54:55 -0400

    update model tracing method

--- code/cpp_test/main_cpp_code/model_serialiser_tracing.py
+++ code/cpp_test/main_cpp_code/model_serialiser_tracing.py
@@ -1,35 +1,42 @@
 import torch
 import torch.nn as nn
 import torchvision
+from torch.optim.lr_scheduler import StepLR
 
 # An instance of your model.
-# model = torchvision.models.resnet18()
-
-# checkpoint = torch.load("/data/mrao70/models/checkpoint.pth.tar")
-# state_dict = checkpoint["state_dict"]
-# state_dict = {key.replace('module.', ''): value for key, value in state_dict.items()}
-
-# model.load_state_dict(state_dict)
-
-# model = model.module if isinstance(model, nn.DataParallel) else model
-
-checkpoint = torch.load(args.resume)
 model = torchvision.models.resnet18()
-optimizer = torch.optim.SGD(
-        model.parameters(),
-        args.lr,
-        momentum=args.momentum,
-        weight_decay=args.weight_decay,
-    )
+model = torch.nn.DataParallel(model).cuda()
+checkpoint = torch.load("/mnt/c/Users/mayur/Downloads/checkpoint.pth.tar")
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
 scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-model.load_state_dict(checkpoint["state_dict"])
+model.load_state_dict(checkpoint["state_dict"], strict=False)
+torch.save(model.module.state_dict(), 'non-parallel-model.pth')
 optimizer.load_state_dict(checkpoint["optimizer"])
 scheduler.load_state_dict(checkpoint["scheduler"])
 
-model.eval()
 
-example = torch.rand(1, 3, 224, 224)
+model_np = torchvision.models.resnet18()
+model_np = model_np.cuda()
+model_np.load_state_dict(torch.load('non-parallel-model.pth'), strict=False)
+model_np = model_np.eval()
 
-traced_script_module = torch.jit.trace(model, example)
-
+example = torch.rand(1, 3, 224, 224).cuda()
+traced_script_module = torch.jit.trace(model_np, example)
 traced_script_module.save("traced_resnet_model.pt")
+
+# ------------------------
+
+# model = AnyNet(args)
+# model = nn.DataParallel(model).cuda()
+# checkpoint = torch.load('/content/checkpoint/kitti2015_ck/checkpoint.tar')
+# model.load_state_dict(checkpoint['state_dict'], strict=False)
+# torch.save(model.module.state_dict(), 'non-parallel-model.pth')
+
+# model_np = AnyNet(args)
+# model_np = model_np.cuda()
+# model_np.load_state_dict(torch.load('non-parallel-model.pth'), strict=False)
+# model_np = model_np.eval()
+
+# example = [torch.rand(1, 3, 1280, 720).cuda(), torch.rand(1, 3, 1280, 720).cuda()]
+# traced_model = torch.jit.trace(model_np, example_inputs=example)
+# traced_model.save("anynet_1280x720_b1.traced.pt")
