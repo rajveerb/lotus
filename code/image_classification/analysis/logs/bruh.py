7d427d89892b39cfa8357127bd277d1074da42fe
rajveerb <46040700+rajveerb@users.noreply.github.com>
2024-03-23 23:19:09 -0500

    deleted some files

--- code/image_classification/analysis/logs/bruh.py
+++
@@ -1,9 +0,0 @@
-with open('code/image_classification/analysis/logs/imagenet_run.log','r') as f:
-    lines = f.readlines()
-    super_sum = 0
-    for line in lines:
-        if 'Elapsed Time: ' in line:
-            time = float(line.split('Elapsed Time: ')[1][:-2])
-            super_sum += time
-    print(super_sum/3600)
-            
