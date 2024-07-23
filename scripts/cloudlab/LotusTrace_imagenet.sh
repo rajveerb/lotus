5a99788639e6179279010478048f045723ffec1b
rajveerb <46040700+rajveerb@users.noreply.github.com>
2024-07-23 16:59:48 -0500

    renamed exp run script for lotustrace functioning

--- scripts/cloudlab/LotusTrace_imagenet.sh
+++ scripts/cloudlab/LotusTrace_imagenet.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 
-result_dir="/mydata/Lotus/lotustrace_imagenet_vary_batch_and_gpu"
+result_dir="/mydata/iiswc24/lotus/lotustrace_result"
 e2e_log_dir="${result_dir}/e2e"
 python_path=$(which python)
 program_path="code/image_classification/code/pytorch_main.py"
 time_binary="/usr/bin/time"
-dataset_dir="/mydata/Lotus/imagenet"
+dataset_dir="/mydata/iiswc24/imagenet"
 
 
-batch_sizes=("128" "256" "512" "1024")
-num_gpus=("1" "2" "3" "4")
+batch_sizes=("1024")
+num_gpus=("4")
 num_epochs=1
 time_format="wall(s),user(s),kernel(s)\n%e,%U,%S"
 
@@ -37,8 +37,8 @@ do
 
         run_result_dir="${result_dir}/b${batch_size}_gpu${num_gpu}"
         # check if result directory exists
-        if [ ! -d ${result_dir} ]; then
-            mkdir -p ${result_dir}
+        if [ ! -d ${run_result_dir} ]; then
+            mkdir -p ${run_result_dir}
         fi
         echo "Result for run with batch size ${batch_size} and num_gpu ${num_gpu} will be stored in ${run_result_dir}" 
         ${time_binary} --quiet --format=${time_format} -o "${e2e_log_dir}/lotustrace_log_b${batch_size}_gpu${num_gpu}.log" ${python_path} ${program_path} ${dataset_dir} -b ${batch_size} --gpus ${num_gpu} -j ${num_gpu} --epochs ${num_epochs} --log-train-file ${run_result_dir}/lotustrace_log --val-loop 0;
