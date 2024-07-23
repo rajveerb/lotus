1ac47181ef08ddd5bf6c63dc5fccd20b8774a3da
rajveerb <46040700+rajveerb@users.noreply.github.com>
2024-07-23 15:45:19 -0500

    setup script for lotustrace and torchvision

---
+++ lotustrace_torchvision_setup.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+echo "Installing in $(which python)"
+
+pushd code/LotusTrace
+conda install -y cmake ninja
+pip install -r requirements.txt
+conda install -y mkl mkl-include
+git submodule sync
+git submodule update --init --recursive --depth 1
+# Below command can cause issues
+export CMAKE_PREFIX_PATH=$(dirname $(dirname $(which conda)))
+echo "CMAKE_PREFIX_PATH is set to $CMAKE_PREFIX_PATH, it should be set to dir which contains the conda installation"  
+sudo apt install -y g++
+REL_WITH_DEB_INFO=1 MAX_JOBS=8 python setup.py install
+popd
+# Sanity check
+pip list | grep "torch" | grep "2.0.0a0"
+
+
+pushd code/torchvision
+conda install -y -c conda-forge libjpeg-turbo
+conda install -y pillow=10.3.0
+python setup.py install
+popd
+echo "Finished!"
