0e7966aefb283c2e2bb99e01cf06543136977097
rajveerb <46040700+rajveerb@users.noreply.github.com>
2024-07-30 19:05:15 -0500

    update scripts (cleaner)

--- install_lotustrace.sh
+++ install_lotustrace.sh
@@ -1,16 +1,17 @@
-# !/bin/bash
+#!/bin/bash
 echo "Installing in $(which python)"
 
 pushd code/LotusTrace
 conda install -y cmake ninja
 pip install -r requirements.txt
 conda install -y mkl mkl-include
+conda install -c pytorch magma-cuda110 -y
 git submodule sync
 git submodule update --init --recursive --depth 1
 # Below command can cause issues
 export CMAKE_PREFIX_PATH=$(dirname $(dirname $(which conda)))
 echo "CMAKE_PREFIX_PATH is set to $CMAKE_PREFIX_PATH, it should be set to dir which contains the conda installation"  
 sudo apt install -y g++
-REL_WITH_DEB_INFO=1 MAX_JOBS=$(nproc) CC=/usr/bin/gcc-7 CXX=/usr/bin/g++-7 python setup.py install
+TORCH_CUDA_ARCH_LIST="8.6" REL_WITH_DEB_INFO=1 python setup.py install
 popd
 echo "Finished!"
--- install_torchvision.sh
+++ install_torchvision.sh
@@ -1,8 +1,7 @@
-# !/bin/bash
+#!/bin/bash
 echo "Installing in $(which python)"
 
 pushd code/torchvision
-conda install -y -c conda-forge libjpeg-turbo
 conda install -y pillow=10.3.0
 python setup.py install
 popd
