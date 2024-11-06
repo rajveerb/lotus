7188a49f1bd3bac66a6cf28b8c79a5dc0999d176
Zachary Robert Yahn <zyahn3@kepler2.cc.gatech.edu>
2024-11-06 10:43:52 -0500

    Organized real-time under code/lotus_rt

---
+++ code/lotus_rt/log_mode.sh
@@ -0,0 +1,6 @@
+cp code/lotus_rt/log_save/pytorch_main.py code/image_classification/code/pytorch_main.py
+cp code/lotus_rt/log_save/dataloader.py /home/zyahn3/anaconda3/envs/lotus/lib/python3.10/site-packages/torch/utils/data/dataloader.py
+cp code/lotus_rt/log_save/transforms.py /home/zyahn3/anaconda3/envs/lotus/lib/python3.10/site-packages/torchvision-0.15.1a0+911e65b-py3.10-linux-x86_64.egg/torchvision/transforms/transforms.py
+cp code/lotus_rt/log_save/folder.py /home/zyahn3/anaconda3/envs/lotus/lib/python3.10/site-packages/torchvision-0.15.1a0+911e65b-py3.10-linux-x86_64.egg/torchvision/datasets/folder.py
+cp code/lotus_rt/log_save/fetch.py /home/zyahn3/anaconda3/envs/lotus/lib/python3.10/site-packages/torch/utils/data/_utils/fetch.py
+cp code/lotus_rt/log_save/worker.py /home/zyahn3/anaconda3/envs/lotus/lib/python3.10/site-packages/torch/utils/data/_utils/worker.py
